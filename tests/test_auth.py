cc5b007c06667365e0804809abe7a2cd61f655da
Bartosz Telenczuk <bartosz.telenczuk@gmail.com>
2020-01-22 21:40:36 +0100

    add real test case

--- tests/test_auth.py
+++ tests/test_auth.py
@@ -2,14 +2,33 @@
 
 from django.test import TestCase
 from rest_framework.test import APIClient
+from users.models import User
+from rest_framework.authtoken.models import Token
 
 class AuthTestCase(TestCase):
     def setUp(self):
         self.client = APIClient()
 
+    @classmethod
+    def setUpTestData(cls):
+        user = User.objects.create(
+            username='testuser', 
+            password='testpassword',
+            email='test@email.com',
+            is_active=True,
+            is_superuser=True)
+
+        token = Token.objects.create(user=user)
+        cls.token = token.key
+
     def test_token_auth(self):
         """Test token authentification."""
 
         request = self.client.get("/users/")
+        assert request.status_code == 401
 
+        request = self.client.get(
+            '/users/',
+            HTTP_AUTHORIZATION="Token {}".format(self.token))
+        assert request.status_code == 200
 
