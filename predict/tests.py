c92f1e39b4c85bf4097ebfadd4ecba647c36f458
Bartosz Telenczuk <bartosz.telenczuk-ext@aphp.fr>
2020-02-03 14:52:41 +0100

    autoformatting with black

--- predict/tests.py
+++ predict/tests.py
@@ -6,34 +6,35 @@ import json
 
 # Create your tests here.
 class TestPredictAPI(TestCase):
-
     @classmethod
     def setUpTestData(cls):
         user = User.objects.create(
-            username='testuser', 
-            password='testpassword',
-            email='test@email.com',
+            username="testuser",
+            password="testpassword",
+            email="test@email.com",
             is_active=True,
-            is_superuser=True)
+            is_superuser=True,
+        )
 
         token = Token.objects.create(user=user)
         cls.token = token.key
-
 
     @mock.patch("redis.Redis.rpush")
     @mock.patch("redis.Redis.mget")
     def test_predict_post(self, mget, rpush):
         mget.return_value = [b'{"labels":["XXXTEST"]}']
 
-        response = self.client.post("/predict/",
-                data=json.dumps({'inputs': [{"text": "Test"}]}),
-            content_type='application/json',
-            HTTP_AUTHORIZATION="Token {}".format(self.token))
+        response = self.client.post(
+            "/predict/",
+            data=json.dumps({"inputs": [{"text": "Test"}]}),
+            content_type="application/json",
+            HTTP_AUTHORIZATION="Token {}".format(self.token),
+        )
 
         self.assertEqual(response.status_code, 200)
         self.assertEqual(
-            response.json(),
-            {"predictions": [{"ccam_codes": ["XXXTEST"]}]})
+            response.json(), {"predictions": [{"ccam_codes": ["XXXTEST"]}]}
+        )
 
     @mock.patch("redis.Redis.rpush")
     @mock.patch("redis.Redis.mget")
@@ -44,18 +45,28 @@ class TestPredictAPI(TestCase):
             b'{"labels": ["ZZZTEST"]}',
         ]
 
-        response = self.client.post("/predict/",
-                data=json.dumps({'inputs': [
-                    {"text": "Test 1"},
-                    {"text": "Test 2"},
-                    {"text": "Test 3"},
-                    ]}),
-            content_type='application/json',
-            HTTP_AUTHORIZATION="Token {}".format(self.token))
+        response = self.client.post(
+            "/predict/",
+            data=json.dumps(
+                {
+                    "inputs": [
+                        {"text": "Test 1"},
+                        {"text": "Test 2"},
+                        {"text": "Test 3"},
+                    ]
+                }
+            ),
+            content_type="application/json",
+            HTTP_AUTHORIZATION="Token {}".format(self.token),
+        )
 
-        self.assertEqual(response.json(),
-            {"predictions": [
-                {"ccam_codes": ["XXXTEST"]},
-                {"ccam_codes": ["YYYTEST"]},
-                {"ccam_codes": ["ZZZTEST"]},
-                ]})
+        self.assertEqual(
+            response.json(),
+            {
+                "predictions": [
+                    {"ccam_codes": ["XXXTEST"]},
+                    {"ccam_codes": ["YYYTEST"]},
+                    {"ccam_codes": ["ZZZTEST"]},
+                ]
+            },
+        )
