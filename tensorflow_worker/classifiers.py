d3259cf70be61bc76220d1e950a9f2de3421e7ce
Bartosz Telenczuk <bartosz.telenczuk-ext@aphp.fr>
2020-02-17 15:18:13 +0100

    reorganise tests directory

--- tensorflow_worker/classifiers.py
+++ tensorflow_worker/classifiers.py
@@ -17,16 +17,32 @@ class BertCCAMClassifier:
     def load_model(self, models_dir):
         "Load model."
 
-        with open(os.path.join(models_dir, 'model_mapping.json')) as fid:
+        with open(os.path.join(models_dir, "model_mapping.json")) as fid:
             models_conf = json.load(fid)
-        path_to_model = os.path.join(models_dir, models_conf['ccam_models'][0]['path'])
+        path_to_model = os.path.join(models_dir, models_conf["ccam_models"][0]["path"])
 
         self.model = CamembertForMultilabelClassification.from_pretrained(path_to_model)
         self.tokenizer = CamembertTokenizer.from_pretrained(path_to_model)
         self.encoder = joblib.load(os.path.join(path_to_model, "encoder.joblib"))
 
+    def _predict(self, valid_documents):
+
+        tokens = self.tokenizer.batch_encode_plus(
+            valid_documents,
+            max_length=512,
+            pad_to_max_length=True,
+            add_special_tokens=True,
+            return_tensors="tf",
+            return_attention_masks=True,
+        )
+        output = self.model(tokens)
+        indicators = output.numpy() > 0.5
+        labels = self.encoder.inverse_transform(indicators)
+
+        return labels
+
     def predict(self, documents):
-        """Make prediction for documents. 
+        """Make prediction for documents.
         
         Return list of tuples (multiple labels per document)"""
 
@@ -43,17 +59,8 @@ class BertCCAMClassifier:
                 valid_ids.append(i)
 
         logger.info("classifier received %d valid inputs", len(valid_documents))
-        tokens = self.tokenizer.batch_encode_plus(
-            valid_documents,
-            max_length=512,
-            pad_to_max_length=True,
-            add_special_tokens=True,
-            return_tensors="tf",
-            return_attention_masks=True,
-        )
-        output = self.model(tokens)
-        indicators = output.numpy() > 0.5
-        labels = self.encoder.inverse_transform(indicators)
+
+        labels = self._predict(valid_documents)
 
         for i, l in zip(valid_ids, labels):
             results[i]["labels"] = l
