59ed06f20dbda6c5970dbd2458253509f8867927
Bartosz Telenczuk <bartosz.telenczuk-ext@aphp.fr>
2020-02-18 13:10:07 +0100

    test choice of ccam model

--- tensorflow_worker/test_classifier.py
+++ tensorflow_worker/test_classifier.py
@@ -22,11 +22,17 @@ class DummyClassifierTest(TestCase):
 
     def test_two_phase_predict(self):
         """Test two phase prediction: cmd -> ccam"""
-        
+
         classifier = BertCCAMClassifier()
         classifier.load_model("models")
         prediction = classifier.predict(["bert"])
         self.assertEqual(prediction, [{"labels": ("C",)}])
+
+        prediction = classifier.predict(["bartosz", "bert"])
+        self.assertEqual(prediction, [{"labels": ("B",)}, {"labels": ("C",)}])
+
+        prediction = classifier.predict(["bert", "bartosz"])
+        self.assertEqual(prediction, [{"labels": ("C",)}, {"labels": ("B",)}])
 
     def test_validate_input_data(self):
 
