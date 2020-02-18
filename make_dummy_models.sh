140e13366f56a266da1424a478981d1198bff2f6
Bartosz Telenczuk <bartosz.telenczuk-ext@aphp.fr>
2020-02-18 13:07:22 +0100

    add second ccam model

--- make_dummy_models.sh
+++ make_dummy_models.sh
@@ -1,2 +1,3 @@
 python make_dummy_model.py tensorflow_worker/tests/data/dummy_ccam_train_dataset.csv --model-dir models/dummy_ccam_model/ --multi-label
+python make_dummy_model.py tensorflow_worker/tests/data/dummy_ccam_train_dataset_cmd_2.csv --model-dir models/dummy_ccam_model_cmd_2/ --multi-label
 python make_dummy_model.py tensorflow_worker/tests/data/dummy_cmd_train_dataset.csv --model-dir models/dummy_cmd_model/
---
+++ tensorflow_worker/test_classifier.py
@@ -0,0 +1,43 @@
+from unittest import TestCase
+from tensorflow_worker.classifiers import BertCCAMClassifier
+import os
+
+
+class DummyClassifierTest(TestCase):
+    def test_predict(self):
+        """Test prediction by dummy BERT classifier."""
+
+        classifier = BertCCAMClassifier()
+        classifier.load_model("models")
+        prediction = classifier.predict(["bartosz"])
+        self.assertEqual(prediction, [{"labels": ("B",)}])
+
+        # with multiple labels
+        prediction = classifier.predict(["ala bert"])
+        self.assertEqual(prediction, [{"labels": ("A", "B")}])
+
+        # in a batch
+        prediction = classifier.predict(["bartosz", "adam"])
+        self.assertEqual(prediction, [{"labels": ("B",)}, {"labels": ("A",)}])
+
+    def test_two_phase_predict(self):
+        """Test two phase prediction: cmd -> ccam"""
+        
+        classifier = BertCCAMClassifier()
+        classifier.load_model("models")
+        prediction = classifier.predict(["bert"])
+        self.assertEqual(prediction, [{"labels": ("C",)}])
+
+    def test_validate_input_data(self):
+
+        classifier = BertCCAMClassifier()
+        classifier.load_model("models")
+        prediction = classifier.predict([5, "barosz"])
+
+        self.assertEqual(
+            prediction,
+            [
+                {"labels": ("ERROR",), "error_message": "wrong document format"},
+                {"labels": ("B",)},
+            ],
+        )
