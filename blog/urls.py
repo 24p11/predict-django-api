859d09517ee061d4c0999e23243cbd837f14710e
Bartosz Telenczuk <bartosz.telenczuk-ext@aphp.fr>
2020-03-02 16:12:28 +0100

    remove unused packages

--- blog/urls.py
+++
@@ -1,9 +0,0 @@
-from django.conf.urls import url
-from django.urls import path
-
-from blog import views
-
-urlpatterns = [
-    url(r'^$', views.list_articles, name='articles'),
-    path('<uuid:uuid>/', views.get_article, name='article'),
-]
